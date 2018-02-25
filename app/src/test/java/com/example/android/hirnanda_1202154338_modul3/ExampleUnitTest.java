54462fded2b4fa4fc7d8199418c60f113c0441b3
Kem <putrihirnanda97@gmail.com>
2018-02-25 23:14:21 +0700

    kem

---
+++ app/src/test/java/com/example/android/hirnanda_1202154338_modul3/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.android.hirnanda_1202154338_modul3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
